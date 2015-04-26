ba0c00bd4d181472af81b2e1ed4240c49bb39a46
Alfonso Ruzafa <superruzafa@gmail.com>
2015-04-26 18:18:04 +0000

    Fix package for make distcheck

--- src/package.h
+++
@@ -1,6 +0,0 @@
-#define BEGUILE_NAME "Beguile"
-#define BEGUILE_VERSION "0.1.0"
-#define BEGUILE_AUTHOR "Alfonso Ruzafa"
-#define BEGUILE_EMAIL  "superruzafa@gmail.com"
-#define BEGUILE_BRAND "Beguile, a BDD framework for C"
-
