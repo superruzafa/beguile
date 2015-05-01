9b4465d45fd3faa88ffedb8b3dab6082838e2b2f
Alfonso Ruzafa <superruzafa@gmail.com>
2015-05-01 22:27:31 +0000

    Added compilation info header

--- src/beguile.h
+++ src/beguile.h
@@ -1,3 +1,32 @@
+/* Beguile 0.2.0, a BDD framework for C
+ *
+ * Compiled on 2015-05-01 22:27:11 with support for
+ *   (x) English language
+ *   (x) Spanish language
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Alfonso Ruzafa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __BEGUILE_H__
 #define __BEGUILE_H__
 
@@ -37,6 +66,7 @@ BeguileGlobalVars beguile_global_vars = {1, 1, NULL};
 #define BEGUILE_AUTHOR "Alfonso Ruzafa"
 #define BEGUILE_EMAIL  "superruzafa@gmail.com"
 #define BEGUILE_BRAND "Beguile, a BDD framework for C"
+#define BEGUILE_URL "https://github.com/superruzafa/beguile.git"
 
 #define BEGUILE_PRINT(...) (beguile_global_vars.output_enabled ? printf(__VA_ARGS__) : 0)
 #define BEGUILE_FLUSH (beguile_global_vars.output_enabled ? fflush(stdout) : 0)
